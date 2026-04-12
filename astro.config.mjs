27665b603c107a79e0d2d3d339d8cd263ff590b2
marco-trento <trento.marco23@gmail.com>
2026-04-12 14:05:58 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://example.com'
+  site: 'https://marco-trento.github.io',
+  base: '/Marco_portfolio',
 });
